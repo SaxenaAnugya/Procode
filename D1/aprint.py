8bd4ec00a151072c84ca44cd744c337ed0bbd7c7
Anugya Saxena <findanugya@gmail.com>
2024-12-07 17:42:59 +0530

    Add files via upload

---
+++ D1/aprint.py
@@ -0,0 +1,9 @@
+import array as arr
+#from array import *(asterisk for all the functions from this module)
+
+v=arr.array('i',[1,2,2,3,4])  
+#first array is imported second array is the module we took from that array
+
+
+for i in range(5):
+    print(v[i])
