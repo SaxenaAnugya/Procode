595f678921e39836233e6b7ed26793107a77a6d7
Anugya Saxena <findanugya@gmail.com>
2024-12-07 17:44:10 +0530

    clean.py

--- aprint.py
+++
@@ -1,9 +0,0 @@
-import array as arr
-#from array import *(asterisk for all the functions from this module)
-
-v=arr.array('i',[1,2,2,3,4])  
-#first array is imported second array is the module we took from that array
-
-
-for i in range(5):
-    print(v[i])
